637e3aaf1b7715682dbc59b7d70a6667fcc7d106
smath <smathewslynch@gmail.com>
2024-02-10 23:32:28 +0000

    Created the DynamicArrayList with an array of strings for the data, int num elements to count the total of the array and capacity to create the array with 10 elements
    created constructor
    
    Added the following methods size,get,indexOf,grow,add

--- src/main/java/utils/DynamicArrayList.java
+++ src/main/java/utils/DynamicArrayList.java
@@ -9,10 +9,47 @@ public class DynamicArrayList {
     private int numElements;
     private static int capacity = 10;
 
+
     public DynamicArrayList() {
         data = new String[capacity];
         numElements = 0;
     }
 
+    public int size() {
+        return numElements;
+    }
 
+    public String get(int pos) {
+        if (pos < 0 || pos >= numElements) {
+            throw new IndexOutOfBoundsException("Invalid position");
+        }
+        return data[pos];
+    }
+
+    public int indexOf(String element) {
+        for (int i = 0; i < numElements; i++) {
+            if (data[i].equals(element)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    //in order to create an add method we must first create a grow method in case the array is full
+    // or use arraycopy, but I went with the grow array
+    private void grow(){
+        //could have created an expansion facter variable but i just decided to use the capacity variable
+        String [] tempArray = new String[data.length + capacity];
+        for(int i = 0; i < data.length; i++){
+            tempArray[i] = data[i];
+        }
+
+        data = tempArray;
+    }
+    public boolean add(String element) {
+        if (numElements == data.length) {
+            grow();
+        }
+        data[numElements++] = element;
+        return true;
+    }
 }
